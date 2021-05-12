e6f9c76e5646eb3b2863e497456affb286b04452
Tsoiffer <tsoiffer@frba.utn.edu.ar>
2021-05-12 16:52:03 -0300

    subo la entrega del tp_monopoly finalizando las acciones ultimaRonad y jugoFinal

--- Haskell/src/tp_monopoly.hs
+++ Haskell/src/tp_monopoly.hs
@@ -9,7 +9,7 @@ data Persona = UnaPersona
     dinero :: Int,
     tactica :: String,
     propiedades :: [Propiedad],
-    acciones :: [(Accion)]
+    acciones :: [Accion]
   }
   deriving (Show)
 
@@ -22,35 +22,48 @@ recoleta = ("recoleta", 550)
 
 -- JUGADORES
 carolina :: Persona
-carolina = UnaPersona "Carolina" 500 "Accionista" [("capilla sixtina", 100), ("tomasss", 200)] [pasarPorElBanco, pagarAAccionistas]
+carolina = UnaPersona "Carolina" 500 "Accionista" [("capilla sixtina", 100), ("Casa Caro", 200)] [pasarPorElBanco, pagarAAccionistas]
 
 manuel :: Persona
 manuel = UnaPersona "Manuel" 500 "Oferente singular" [("torre de pizza", 100)] [pasarPorElBanco, enojarse]
 
-pasarPorElBanco :: Persona -> Persona
+--FUNCIONES
+pasarPorElBanco :: Accion
 pasarPorElBanco jugador = jugador {tactica = "Comprador compulsivo", dinero = dinero jugador + 40}
 
-enojarse :: Persona -> Persona
+enojarse :: Accion
 enojarse jugador = jugador {acciones = acciones jugador ++ [gritar], dinero = dinero jugador + 50}
 
-gritar :: Persona -> Persona
+gritar :: Accion
 gritar jugador = jugador {nombre = "AHHHH" ++ nombre jugador}
 
-subastar :: Persona -> Persona
+subastar :: Accion
 subastar jugador
   | tactica jugador == "Oferente singular" = jugador {nombre = "AHHHH" ++ nombre jugador}
   | tactica jugador == "Accionista" = jugador {nombre = "AHHHH" ++ nombre jugador}
   | otherwise = jugador
 
-cobrarAlquileres :: Persona -> Persona
+cobrarAlquileres :: Accion
 cobrarAlquileres jugador = jugador {dinero = dinero jugador + 10 * (sum . (map (ceiling . (/ 150) . snd)) $ (propiedades jugador))}
 
-pagarAAccionistas :: Persona -> Persona
+pagarAAccionistas :: Accion
 pagarAAccionistas jugador
   | tactica jugador == "Accionista" = jugador {dinero = 200 + dinero jugador}
   | otherwise = jugador {dinero = dinero jugador - 100}
 
-hacerBerrinchePor :: Propiedad -> Persona -> Persona
+hacerBerrinchePor :: Propiedad -> Accion
 hacerBerrinchePor propiedad jugador
   | fromIntegral (dinero jugador) >= (snd propiedad) = jugador
   | otherwise = (hacerBerrinchePor propiedad) . gritar $ (jugador {dinero = dinero jugador + 10})
+
+ultimaRonda :: Persona -> Accion
+ultimaRonda jugador = foldl1 (.) (acciones jugador)
+
+juegoFinal :: Persona -> Persona -> String
+juegoFinal jugador1 jugador2
+  | (dineroFinal jugador1) > (dineroFinal jugador2) = "Gano " ++ nombre jugador1 ++ "!"
+  | (dineroFinal jugador2) > (dineroFinal jugador1) = "Gano " ++ nombre jugador2 ++ "!"
+  | otherwise = "Empate!"
+
+dineroFinal :: Persona -> Int
+dineroFinal jugador = dinero . (ultimaRonda jugador) $ jugador
